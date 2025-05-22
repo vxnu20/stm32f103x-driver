f6c29072a05353c4362a85dc09a9eab8521e91ba
Vishnu <vishnu012@protonmail.com>
2025-05-22 17:12:20 +0530

    gpio-changed to driver function calls

--- main.c
+++ main.c
@@ -7,14 +7,11 @@ int main()
     /* enable clock for PORT c*/
     RCC->APB2ENR |= (1<<4);
     
-    /* blink example */
-    GPIO_PORTC->CRH &= ~(0xFF << 20); // clearing Mode and CNF bits
-    GPIO_PORTC->CRH |= (0x02 << 20); // setting the mode to output 
-    /* keeping CNF as 00 */
+    gpio_set_mode(GPIO_PORTC, 13, GPIO_MODE_OUT2MHZ, GN_PUSH_PULL);
     
     while(1)
     {
-        GPIO_PORTC->ODR ^= (1<<13);
+        gpio_pin_toggle(GPIO_PORTC, 13);
     }
 
     return 0;
