5fed584bcb372558e470c467c213b21a3a4dfd70
Vishnu <vishnu012@protonmail.com>
2025-07-22 15:58:09 +0530

    i2c -> init function, not tested

--- include/stm32f103x_i2c.h
+++ include/stm32f103x_i2c.h
@@ -5,16 +5,19 @@
 #include <stdint.h>
 
 /* macros of i2c */
-#define I2C_BASE_ADDR       (0x40005400UL)
-#define I2C1_OFFSET         (0x0UL)
-#define I2C2_OFFSET         (0x400UL)
-#define I2C_CR1_PE_SET      (1<<0)
-#define I2C_CR1_SWRST       (1<<15)
-
+#define I2C_BASE_ADDR           (0x40005400UL)
+#define I2C1_OFFSET             (0x0UL)
+#define I2C2_OFFSET             (0x400UL)
+#define I2C_CR1_PE              (1<<0)
+#define I2C_CR1_SWRST           (1<<15)
+#define I2C_CR2_FREQ_POS        (0UL)
+#define I2C_DEFAULT_FREQ_MHZ    (8UL) /* used for config */
+#define I2C_CCR_CCR_POS         (0UL)
+#define I2C_TRISE_TRISE_POS     (0UL)
 
 /* i2c peripherals*/
-#define I2C1                ((i2c_regs *) (I2C_BASE_ADDR + I2C1_OFFSET))
-#define I2C2                ((i2c_regs *) (I2C_BASE_ADDR + I2C2_OFFSET))
+#define I2C1                    ((i2c_regs *) (I2C_BASE_ADDR + I2C1_OFFSET))
+#define I2C2                    ((i2c_regs *) (I2C_BASE_ADDR + I2C2_OFFSET))
 
 /* structure for the i2c registers */
 typedef struct {
@@ -32,9 +35,14 @@ typedef struct {
 /* i2c config paramters */
 typedef struct {
     i2c_regs* i2c;
+    /* freq in mhz */
+    uint8_t pfrequency;
+    uint8_t clock_frequency;
+    uint8_t rise_time;
 }i2c_config;
 
 /* function prototypes */
 void i2c_init(i2c_config);
+uint8_t i2c_byte_read(i2c_config);
 
 #endif
--- src/stm32f103x_i2c.c
+++ src/stm32f103x_i2c.c
@@ -3,5 +3,21 @@
 
 void i2c_init(i2c_config config)
 {
-    
+    /* enter reset mode - required for reinit after lockup */
+    config.i2c->CR1 |= I2C_CR1_SWRST;
+    /* clear the reset mode */
+    config.i2c->CR1 &= ~I2C_CR1_SWRST; 
+    /* set the frequency */
+    config.i2c->CR2 |= config.frequency << I2C_CR2_FREQ_POS;
+    /* set the clock speed, SM or FM */
+    config.i2c->CCR |= config.clock_frequency << I2C_CCR_CCR_POS;
+    /* set the raise time */
+    config.i2c->TRISE |= config.rise_time << I2C_TRISE_TRISE_POS;
+    /* enable I2C peripheral */
+    config.i2c->CCR1 |= I2C_CR1_PE;
+}
+
+uint8_t i2c_read(i2c_config config)
+{
+
 }
