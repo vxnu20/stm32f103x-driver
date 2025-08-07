5c97f9460affa5f4794fe56942052b0e15e5139f
Vishnu <vishnu012@protonmail.com>
2025-08-07 15:52:57 +0530

    spi -> header with basic info

---
+++ include/stm32f103x_spi.h
@@ -0,0 +1,36 @@
+#ifndef STM32F103X_SPI_H
+#define STM32F103X_SPI_H
+
+/* required header files */
+<stdint.h>
+
+/* macros of spi */
+#define SPI1_BASE_ADDR      (0x40013000UL)
+#define SPI2_BASE_ADDR      (0x40003800UL)
+
+/* spi peripherals */
+#define SPI1        ((spi_regs*) SPI1_BASE_ADDR)
+#define SPI2        ((spi_regs*) SPI2_BASE_ADDR)
+
+/* structure for spi registers */
+typdef struct {
+    volatile uint32_t CR1;      /* control register 1 */
+    volatile uint32_t CR2;      /* control register 2 */
+    volatile uint32_t SR;       /* status register */
+    volatile uint32_t DR;       /* data register 2 */
+    volatile uint32_t CRCPR;    /* CRC polynomial register */
+    volatile uint32_t RXCRCR;   /* Rx CRC register */
+    volatile uint32_t TXCRCR;   /* Tx CRC register */
+    volatile uint32_t I2SCFGR;  /* I2S config register */
+    volatile uint32_t I2SPR;    /* I2S pre-scalar register */   
+}spi_regs;
+
+/* structure for the config paramters */
+typdef struct {
+    spi_regs* spi;
+}spi_config;
+
+/* function prototypes */
+void spi_init(spi_config);
+
+#endif
---
+++ src/stm32f103x_spi.c
@@ -0,0 +1,6 @@
+#include "stm32f103x_spi.h"
+
+void spi_init(spi_config config)
+{
+
+}
