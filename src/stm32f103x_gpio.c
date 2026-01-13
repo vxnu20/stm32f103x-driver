8fbea4bc3c7920e43f19797b3d7ad2ccf5b1e99a
vxnu20 <vishnu012@pm.me>
2026-01-13 20:00:09 +0530

    bug -> clearing 8 bits instead of 4 inside gpio set mode api causingleading to corruption of adjacent config bits

--- src/stm32f103x_gpio.c
+++ src/stm32f103x_gpio.c
@@ -5,7 +5,7 @@ void gpio_set_mode(gpio_regs* gpio, uint8_t pin, uint8_t mode, uint8_t cnf)
     volatile uint32_t* reg = (pin < 8) ? &gpio->CRL : &gpio->CRH;
     pin = (pin < 8) ? pin : pin - 8;
     
-    *reg &= ~(0xFF << (pin * 4));       /* Clear both CNF and mode bits */
+    *reg &= ~(0xF << (pin * 4));        /* Clear both CNF and mode bits */
     *reg |= (mode << (pin * 4));        /* Set the mode bit */
     *reg |= (cnf << ((pin * 4) + 2));   /* Set the CNF bit */
 }
